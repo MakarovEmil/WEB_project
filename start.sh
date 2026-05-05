7e63ecf00a69acb02559c7a0a917328aa3a9d896
user <makarovemil660@gmail.com>
2026-05-05 20:00:19 +0300

    Commit final

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-cd WEB_project
 export PORT=5000
 
 if [ -f "requirements.txt" ]; then
     echo "Installing dependencies..."
-    pip install --user -r requirements.txt || \
-    pip install -r requirements.txt || \
+    pip3 install -r requirements.txt || \
     echo "Pip install failed, but continuing..."
 fi
 
 echo "Starting application..."
-python main.py
+python3 main.py
